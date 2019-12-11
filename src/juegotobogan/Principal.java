ec4ff6ad1934deb454d1350948d24c46cdc29f6f
jonathan.arroyo <jonathan.arroyo@AMK-0025.amerikatis.com>
2019-12-11 18:09:41 -0500

    En desarrollo

--- src/juegotobogan/Principal.java
+++ src/juegotobogan/Principal.java
@@ -32,9 +32,10 @@ public class Principal {
 	}
 
 	private void iniciar() throws NumberFormatException, IOException {
+
 		int n, a = 0, b;
 		int[] jugadores = new int[6];
-		boolean[] miss = new boolean[6];
+		boolean[] fallos = new boolean[6];
 		int[] tiros = new int[1001];
 		TreeMap<Integer, Integer> escalera = new TreeMap<Integer, Integer>();
 
@@ -59,44 +60,45 @@ public class Principal {
 		n = Integer.parseInt(br.readLine());
 
 		for (; n > 0 && n < 6; n--) {
-			st = new StringTokenizer(br.readLine());
-			while (st.hasMoreTokens()) {
+
+			while (true) {
+				st = new StringTokenizer(br.readLine());
+
 				a = Integer.parseInt(st.nextToken());
 				b = Integer.parseInt(st.nextToken());
 
+				if (a == 0 && b == 0) {
+					break;
+				}
 				escalera.put(a, b);
 			}
 
 			while (true) {
-				st = new StringTokenizer(br.readLine());
-				while (st.hasMoreTokens()) {
-					a = Integer.parseInt(st.nextToken());
-					extra.put(Math.abs(a), a);
-					if (a == 0) {
-						break;
-					}
-				}
-				
+				a = Integer.parseInt(br.readLine());
 				if (a == 0) {
 					break;
-				}				
+				}
+				extra.put(Math.abs(a), a);
+
 			}
 
 			cur_jugadores = 0;
 			for (int i = 0; i < 6; i++) {
-				miss[i] = false;
+				fallos[i] = false;
 				jugadores[i] = 0;
 			}
 
 			cur_tiros = 0;
 			while (true) {
-				if (miss[cur_jugadores]) {
-					miss[cur_jugadores] = false;
+				if (fallos[cur_jugadores]) {
+					fallos[cur_jugadores] = false;
 					cur_jugadores++;
-					if (cur_jugadores >= n)
+					if (cur_jugadores >= n) {
 						cur_jugadores = 0;
+					}
 					continue;
 				}
+
 				if (jugadores[cur_jugadores] + tiros[cur_tiros] > 100) {
 					cur_tiros++;
 					cur_jugadores++;
@@ -109,7 +111,7 @@ public class Principal {
 				jugadores[cur_jugadores] += tiros[cur_tiros];
 				cur_tiros++;
 
-				if (escalera.get(jugadores[cur_jugadores]) != escalera.lastEntry().getValue()) {
+				if (escalera.containsKey(jugadores[cur_jugadores])) {
 					jugadores[cur_jugadores] = escalera.get(jugadores[cur_jugadores]);
 					if (jugadores[cur_jugadores] == 100) {
 						ganador = cur_jugadores + 1;
@@ -117,19 +119,21 @@ public class Principal {
 					}
 				}
 
-				if (extra.get(jugadores[cur_jugadores]) != extra.lastEntry().getValue()) {
+				if (extra.containsKey(jugadores[cur_jugadores])) {
 					if (extra.get(jugadores[cur_jugadores]) < 0) {
-						miss[cur_jugadores] = true;
+						fallos[cur_jugadores] = true;
 					} else {
 						cur_jugadores--;
 					}
 				}
+
 				cur_jugadores++;
 				if (cur_jugadores >= n) {
 					cur_jugadores = 0;
 				}
-				System.out.print(ganador);
 			}
+			System.out.print("El Ganador es: ");
+			System.out.print(ganador);
 
 		}
 	}
